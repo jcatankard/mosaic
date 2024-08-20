60766a521235b12440bfe881ac7522a0e66ffa9c
e10110199 <joshua.tankard@vista.com>
2024-08-20 17:13:15 +0200

    rename module. Move best fit to Mosaic class.

--- scoring.py
+++ scoring.py
@@ -1,10 +1,10 @@
-from numba import njit, float64, int64, prange
+from numba import njit, float64, prange
 from numpy.typing import NDArray
 import numpy as np
 
 
 @njit(float64[:, ::1](float64[:, ::1], float64[:, ::1]), cache=True, parallel=True)
-def evaluate(target: NDArray[np.float64], tile_options: NDArray[np.float64]) -> NDArray[np.float64]:
+def scoring(target: NDArray, tile_options: NDArray) -> NDArray:
     """
     :param target: arrays representing sections of the target image that we are trying to represent
     :param tile_options: arrays representing images to for the mosaic from
@@ -18,8 +18,3 @@ def evaluate(target: NDArray[np.float64], tile_options: NDArray[np.float64]) -> 
         for j in range(n_option_images):
             err[i, j] = np.mean((target[i] - tile_options[j]) ** 2)
     return err
-
-
-@njit(int64[::1](float64[:, ::1]), cache=True)
-def best_fit(comparison: NDArray[np.float64]) -> NDArray[np.int64]:
-    return np.argmin(comparison, axis=1)
