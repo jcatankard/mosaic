fa570134de776992bec2b8d21650a48438ad7c53
e10110199 <joshua.tankard@vista.com>
2024-08-20 17:17:24 +0200

    replace named tuple with data class

--- shape.py
+++ shape.py
@@ -1,3 +1,8 @@
-from collections import namedtuple
+from dataclasses import dataclass
 
-Shape = namedtuple('shape', ['height', 'width', 'n_colours'])
+
+@dataclass
+class Shape:
+    height: int
+    width: int
+    n_colours: int = 3
