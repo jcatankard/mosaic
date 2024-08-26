0cd787741702754c7735d72f55b19e266999b324
e10110199 <joshua.tankard@vista.com>
2024-08-26 10:28:20 +0200

    remove real file path

--- image_converter/main.py
+++ image_converter/main.py
@@ -3,7 +3,7 @@ from image_converter import ImageConverter
 
 if __name__ == "__main__":
     ic = ImageConverter()
-    directory = r"C:\Users\e10110199\Downloads\test photos"
+    directory = r"C:\Users\..."
     print("Number of files:", len(ic.find_files(directory)))
 
     ic.batch_convert(directory, new_type="JPEG")
